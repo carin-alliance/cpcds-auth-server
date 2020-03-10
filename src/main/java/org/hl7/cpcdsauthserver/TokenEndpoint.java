a7769554a1d21a55d91edd8e8f9d486dce1bedd6
Benjamin Langley <blangley@mitre.org>
2020-03-10 13:41:34 -0400

    Initial pass at refresh tokens

--- src/main/java/org/hl7/cpcdsauthserver/TokenEndpoint.java
+++ src/main/java/org/hl7/cpcdsauthserver/TokenEndpoint.java
@@ -6,6 +6,7 @@ import java.time.ZoneId;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,11 +39,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/token")
 public class TokenEndpoint {
 
-    @PostMapping(value = "")
+    /**
+     * Enum for types of tokens
+     */
+    public enum TokenType {
+        REFRESH, ACCESS;
+    }
+
+    @PostMapping(value = "", params = { "grant_type", "code", "redirect_uri" })
     public ResponseEntity<String> Token(HttpServletRequest request, @RequestParam(name = "grant_type") String grantType,
             @RequestParam(name = "code") String code, @RequestParam(name = "redirect_uri") String redirectURI) {
         System.out.println("TokenEndpoint::Token:Received request /token?grant_type=" + grantType + "&code=" + code
                 + "&redirect_uri=" + redirectURI);
+        return processRequest(request, grantType, code, redirectURI);
+    }
+
+    @PostMapping(value = "", params = { "grant_type", "refresh_token" })
+    public ResponseEntity<String> Token(HttpServletRequest request, @RequestParam(name = "grant_type") String grantType,
+            @RequestParam(name = "refresh_token") String refreshToken) {
+        System.out.println("TokenEndpoint::RefreshToken:Received request /token?grant_type=" + grantType
+                + "&refresh_token=" + refreshToken);
+        return processRequest(request, grantType, refreshToken, null);
+    }
+
+    private ResponseEntity<String> processRequest(HttpServletRequest request, String grantType, String token,
+            String redirectURI) {
         // Set the headers for the response
         MultiValueMap<String, String> headers = new HttpHeaders();
         headers.add(HttpHeaders.CACHE_CONTROL, "no-store");
@@ -50,6 +71,8 @@ public class TokenEndpoint {
 
         HashMap<String, String> response = new HashMap<String, String>();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        String baseUrl = App.getServiceBaseUrl(request);
 
         // Validate the client is authorized
         String clientId = clientIsAuthorized(request);
@@ -60,32 +83,41 @@ public class TokenEndpoint {
             return new ResponseEntity<String>(gson.toJson(response), headers, HttpStatus.UNAUTHORIZED);
         }
 
-        // Validate the grant_type is authorization_code
-        if (!grantType.equals("authorization_code")) {
+        // Validate the grant_type is authorization_code or refresh_token
+        boolean tokenIsValid = false;
+        if (grantType.equals("authorization_code")) {
+            // Request is to trade authorization_code for access token
+            tokenIsValid = authCodeIsValid(token, baseUrl, redirectURI, clientId);
+        } else if (grantType.equals("refresh_token")) {
+            // Request is to trade refresh_token for access token
+            tokenIsValid = refreshTokenIsValid(token, baseUrl, clientId);
+        } else {
             response.put("error", "invalid_request");
             response.put("error_description", "grant_type must be authorization_code not " + grantType);
             return new ResponseEntity<String>(gson.toJson(response), headers, HttpStatus.BAD_REQUEST);
         }
 
-        String baseUrl = App.getServiceBaseUrl(request);
-        if (authCodeIsValid(code, baseUrl, redirectURI, clientId)) {
-            String token = generateAccessToken(code, baseUrl, clientId);
-            System.out.println("TokenEndpoint::Token:Generated token " + token);
-            if (token != null) {
-                response.put("access_token", token);
+        if (tokenIsValid) {
+            String accessToken = generateToken(token, baseUrl, clientId, UUID.randomUUID().toString(),
+                    TokenType.ACCESS);
+            System.out.println("TokenEndpoint::Token:Generated token " + accessToken);
+            if (accessToken != null) {
+                String jwtId = UUID.randomUUID().toString();
+                response.put("access_token", accessToken);
                 response.put("token_type", "bearer");
                 response.put("expires_in", "3600");
                 response.put("scope", "patient/*.read");
+                response.put("refresh_token", generateToken(token, baseUrl, clientId, jwtId, TokenType.REFRESH));
+                // TODO: post jwtID to database
                 return new ResponseEntity<String>(gson.toJson(response), headers, HttpStatus.OK);
             } else {
                 response.put("error", "invalid_request");
                 response.put("error_description", "Internal server error. Please try again");
-                return new ResponseEntity<String>(gson.toJson(response), headers, HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<String>(gson.toJson(response), headers, HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
             response.put("error", "invalid_grant");
-            response.put("error_description",
-                    "Unable to verify authentication code. Please make sure it is still valid");
+            response.put("error_description", "Unable to verify. Please make sure the code/token is still valid");
             return new ResponseEntity<String>(gson.toJson(response), headers, HttpStatus.BAD_REQUEST);
         }
     }
@@ -127,28 +159,33 @@ public class TokenEndpoint {
     }
 
     /**
-     * Generate an access token for the user with the correct claims. Access token
-     * is valid for 1 hour
+     * Generate an access (or request) token for the user with the correct claims.
+     * Access token is valid for 1 hour
      * 
-     * @param code     - the authorization code from the POST request
-     * @param baseUrl  - the base url of this service
-     * @param clientId - the client id
+     * @param code      - the authorization code from the POST request
+     * @param baseUrl   - the base url of this service
+     * @param clientId  - the client id
+     * @param jwtId     - the unique id for this token
+     * @param tokenType - the type of token to generate
      * @return access token for granted user or null
      */
-    private String generateAccessToken(String code, String baseUrl, String clientId) {
+    private String generateToken(String code, String baseUrl, String clientId, String jwtId, TokenType tokenType) {
         try {
             // Create the access token JWT
             Algorithm algorithm = Algorithm.HMAC256(App.getSecret());
-            Instant oneHour = LocalDateTime.now().plusHours(1).atZone(ZoneId.systemDefault()).toInstant();
-            return JWT.create().withIssuer(baseUrl).withExpiresAt(Date.from(oneHour)).withIssuedAt(new Date())
-                    .withAudience(App.getEhrServer()).withClaim("client_id", clientId).sign(algorithm);
+            String aud = tokenType == TokenType.ACCESS ? App.getEhrServer() : baseUrl;
+            Instant exp = tokenType == TokenType.ACCESS
+                    ? LocalDateTime.now().plusHours(1).atZone(ZoneId.systemDefault()).toInstant()
+                    : LocalDateTime.now().plusDays(30).atZone(ZoneId.systemDefault()).toInstant();
+            return JWT.create().withIssuer(baseUrl).withExpiresAt(Date.from(exp)).withIssuedAt(new Date())
+                    .withAudience(aud).withClaim("client_id", clientId).withJWTId(jwtId).sign(algorithm);
         } catch (JWTCreationException exception) {
             // Invalid Signing configuration / Couldn't convert Claims.
-            System.out.println("TokenEndpoint::generateAccessToken:Unable to generate access token");
+            System.out.println("TokenEndpoint::generateToken:Unable to generate token");
             return null;
         } catch (JWTVerificationException exception) {
             // Invalid code
-            System.out.println("TokenEndpoint::generateAccessToken:Unable to verify code");
+            System.out.println("TokenEndpoint::generateToken:Unable to verify code");
             return null;
         }
     }
@@ -176,6 +213,32 @@ public class TokenEndpoint {
             }
         } catch (JWTVerificationException exception) {
             System.out.println("TokenEndpoint::Authorization code is invalid. Please obtain a new code");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Validate/verify the refresh token is valid
+     * 
+     * @param token   - the refresg token
+     * @param baseUrl - the base URL of this service
+     * @return true if the refresh token is valid and false otherwise
+     */
+    private boolean refreshTokenIsValid(String code, String baseUrl, String clientId) {
+        // TODO: validate the jwt ID is still valid
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(App.getSecret());
+            JWTVerifier verifier = JWT.require(algorithm).withIssuer(baseUrl).withAudience(baseUrl).build();
+            DecodedJWT jwt = verifier.verify(code);
+            String jwtClientId = jwt.getClaim("client_id").asString();
+            if (!clientId.equals(jwtClientId)) {
+                System.out.println(
+                        "TokenEndpoint::Refresh token is invalid. Client ID does not match authorization header");
+                return false;
+            }
+        } catch (JWTVerificationException exception) {
+            System.out.println("TokenEndpoint::Refresh token is invalid. Please reauthorize");
             return false;
         }
         return true;
